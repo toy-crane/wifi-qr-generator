0f031d7e1bfc5ca9347fe14c06c4c006d8b4b97d
toy-crane <alwaysfun2183@gmail.com>
2025-04-24 14:11:46 +0900

    refactor: 불필요한 'Examples' 링크 및 이미지 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,21 +67,6 @@ export default function Home() {
           />
           Learn
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
       </footer>
     </div>
   );
